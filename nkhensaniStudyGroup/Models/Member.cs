68b0d88c967c3532885dfc2084fb5365b4ea4f6f
Nkhensani <notigokhensani@gmail.com>
2025-03-02 06:48:32 +0200

    Added the Model Class

---
+++ nkhensaniStudyGroup/Models/Member.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace nkhensaniStudyGroup.Models
+{
+	public class Member
+	{
+		public int StdNumber { get; set; }
+		public string StdName { get; set; }
+        public string StdSurname { get; set; }
+
+        public string StdEmail { get; set; }
+    }
+}
